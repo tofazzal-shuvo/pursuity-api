71dd38f4c1ba76b8a82b8f10697882e77e39ec9b
tofazzal-shuvo <tofazzalhaque63@gmail.com>
2021-01-11 00:43:05 +0600

    lk

--- api/app/utility/fileUpload.js
+++ api/app/utility/fileUpload.js
@@ -1,7 +1,7 @@
 import { s3bucket } from "../constant/aws";
 
 export const uploadToAws = (file) =>
-  new Promise((resovle, reject) => {
+  new Promise((resovle, reject) => { 
     const params = {
       Bucket: "slashit",
       Key: file.filename,
@@ -39,30 +39,3 @@ export const catchFile = (file) =>
     });
     stream.on("error", reject);
   });
-
-export const businessScriptWriter = async (user) => {
-  const jsCode = `function checkout({valueId, descId, apiKey}) {
-    var amount =
-      document.getElementById(valueId).value ||
-      document.getElementById(valueId).innerText;
-    var desc =
-      document.getElementById(descId).value ||
-      document.getElementById(descId).innerText;
-    var obj = {
-      id: apiKey,
-      amount: amount,
-      desc: desc,
-      src: window.location.href,
-    };
-    var salt = "some string here";
-    var data = btoa(JSON.stringify(obj));
-    window.location.assign("https://ez-pm.herokuapp.com/pay-auth/" + data);
-  }`;
-  const file = {
-    filename: `main.js`,
-    buffers: Buffer.from(jsCode),
-    mimetype: "application/javascript",
-  };
-  await uploadToAws(file);
-  return;
-};
