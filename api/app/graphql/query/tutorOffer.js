20cfe3178fa12084585adbc7b053c8d2696996c2
tofazzal-shuvo <tofazzalhaque63@gmail.com>
2021-01-12 01:23:59 +0600

    kld

--- api/app/graphql/query/tutorOffer.js
+++ api/app/graphql/query/tutorOffer.js
@@ -1,13 +1,15 @@
 import { statusCode } from "../../constant";
 import { TutorOfferModel } from "../../models";
 
-export const FetchTutorOffer = async (_, __, user) => {
-  console.log(user)
+export const FetchTutorOffer = async (_, __, { user }) => {
+  console.log(user);
 
   try {
     const count = await TutorOfferModel.countDocuments({});
-    const result = await TutorOfferModel.find({tutor: user._id}).populate('subject')
-    console.log(result)
+    const result = await TutorOfferModel.find({ tutor: user._id }).populate(
+      "subject"
+    );
+    console.log(user);
     return {
       code: statusCode.OK,
       count,
@@ -16,7 +18,7 @@ export const FetchTutorOffer = async (_, __, user) => {
       success: true,
     };
   } catch (err) {
-    console.log(err)
+    console.log(err);
 
     return {
       code: statusCode.INTERNAL_ERROR,
