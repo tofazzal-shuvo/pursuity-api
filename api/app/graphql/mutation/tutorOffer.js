010ec245c53c9ac572cd6e1927193ad794cc32f7
tofazzal-shuvo <tofazzalhaque63@gmail.com>
2021-01-11 23:12:06 +0600

    hj

--- api/app/graphql/mutation/tutorOffer.js
+++ api/app/graphql/mutation/tutorOffer.js
@@ -4,7 +4,8 @@ import { TutorOfferModel } from "../../models";
 export const AddOffers = async (_, { offer }, user) => {
   try {
     const tutorOffer = await TutorOfferModel.create({ ...offer, tutor: user._id });
-    user.offers.push(tutorOffer._id);
+    const offers = Array.isArray(user.offers) ? user.offers : []
+    user.offers=offers.push(tutorOffer._id);
     await user.save();
     return {
       code: statusCode.CREATED,
